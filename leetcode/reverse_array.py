ef628dcdc0bbdaabac077aab9a746c4dddf1b461
avijeetpandey <avijeetpandey25@gmail.com>
2025-08-22 17:17:04 +0530

    add: reverse array

---
+++ leetcode/reverse_array.py
@@ -0,0 +1,12 @@
+nums = [5, 4, 3, 2, 1]
+
+def reverseArray(nums):
+    left = 0
+    right = len(nums) - 1
+    while left < right:
+        nums[left], nums[right] = nums[right], nums[left]
+        left += 1
+        right -= 1
+    return nums
+
+print(reverseArray(nums))
