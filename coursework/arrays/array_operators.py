75569f500ffff43f939a3f12eae53b4b407d15eb
avijeetpandey <avijeetpandey25@gmail.com>
2025-08-23 16:27:38 +0530

    add: array operators

--- coursework/arrays/array_operators.py
+++ coursework/arrays/array_operators.py
@@ -7,3 +7,16 @@ for i in range(len(brands)):
 # for in range
 for brand in brands:
     print(brand)
+
+# list comprehension
+## list comprehension is a way to create a list quickly
+ten_numbers = [x for x in range(10)]
+print(ten_numbers)
+
+## create a list of cube numbers using list comprehension
+cube_numbers = [x**3 for x in range(10)]
+print(cube_numbers)
+
+## create a static list of numbers using list comprehension
+static_numbers = [5]*10
+print(static_numbers)
