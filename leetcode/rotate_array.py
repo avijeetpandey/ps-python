6f1027326b194cb8e82183ff12ec269adbe0b24f
avijeetpandey <avijeetpandey25@gmail.com>
2025-08-23 18:08:23 +0530

    add: rotate array

---
+++ leetcode/rotate_array.py
@@ -0,0 +1,19 @@
+nums = [1,2,3,5,6,7]
+
+def reverse(nums, start, end):
+    i = start
+    j = end
+    while i < j:
+        nums[i], nums[j] = nums[j] , nums[i]
+        i+=1
+        j-=1
+
+def rotateArray(nums, k):
+    n = len(nums)
+    k = k%n
+    reverse(nums,0,n-1)
+    reverse(nums,0,k-1)
+    reverse(nums,k,n-1)
+
+rotateArray(nums, 3)
+print(nums)
