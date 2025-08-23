e47b763f8a4ca7241a422dcb300cf321f75f1bb0
avijeetpandey <avijeetpandey25@gmail.com>
2025-08-23 16:22:28 +0530

    add: array operators

---
+++ coursework/arrays/array_operators.py
@@ -0,0 +1,9 @@
+brands = ['Toyota', 'BMW', 'Skoda', 'Maruti', 'Suzuki']
+
+# iterating over the brands
+for i in range(len(brands)):
+    print(brands[i])
+
+# for in range
+for brand in brands:
+    print(brand)
