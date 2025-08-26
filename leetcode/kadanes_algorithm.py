8db5035a55e04062b0a1b53454317644c7d0cd18
avijeetpandey <avijeetpandey25@gmail.com>
2025-08-27 01:47:48 +0530

    add: kadane's algorithm

---
+++ leetcode/kadanes_algorithm.py
@@ -0,0 +1,10 @@
+def maximumSumSubarray(nums):
+    max_sum = float('-inf')
+    current_sum = 0
+    for num in nums:
+        current_sum += num
+        if current_sum > max_sum:
+            max_sum = current_sum
+        if current_sum < 0:
+            current_sum = 0
+    return max_sum
